f466dfa9278b5aa32cf5a7bd56c6ca202077d4ee
thine <thinhcubi27@gmail.com>
2021-04-24 10:44:29 +0700

    Jerry,js

---
+++ Jerry.js
@@ -0,0 +1,11 @@
+class Jerry{
+    constructor(name,weight,speed) {
+    this.name=name;
+    this.weight=weight;
+    this.speed=speed;
+    this.status = true;
+    }
+    Say(){
+        console.log("chit chit")
+    }
+}
